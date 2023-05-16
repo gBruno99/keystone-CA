365d07a06623d42bc1b9d69952fa10bd432cdacb
Giacomo <s301311@studenti.polito.it>
2023-05-16 15:05:42 +0200

    Updated my_mbedtls

--- my_mbedtls/my_utils.c
+++ my_mbedtls/my_utils.c
@@ -36,6 +36,30 @@ typedef struct {
     mbedtls_pk_type_t           pk_alg;
 } oid_sig_alg_t;
 
+const oid_sig_alg_t oid_sig_alg[] = // new_impl
+    {
+        {
+            OID_DESCRIPTOR(MBEDTLS_OID_PKCS1_MD5, "md5WithRSAEncryption", "RSA with MD5"),
+            MBEDTLS_MD_MD5,
+            MBEDTLS_PK_RSA,
+        },
+        {
+            OID_DESCRIPTOR(MBEDTLS_OID_PKCS1_SHA224, "sha224WithRSAEncryption", "RSA with SHA-224"),
+            MBEDTLS_MD_SHA224,
+            MBEDTLS_PK_RSA,
+        },
+        {
+            OID_DESCRIPTOR(MBEDTLS_OID_ECDSA_SHA1, "ecdsa-with-SHA1", "ECDSA with SHA1"),
+            MBEDTLS_MD_SHA1,
+            MBEDTLS_PK_ECDSA,
+        },
+        {
+            OID_DESCRIPTOR("\x2B\x65\x70", "ed25519-with-sha3", "ed25519 with sha3"),
+            MBEDTLS_MD_SHA512,
+            MBEDTLS_PK_ED25519,
+        },
+};
+
 FN_OID_GET_OID_BY_ATTR2(mbedtls_oid_get_oid_by_sig_alg,
                         oid_sig_alg_t,
                         oid_sig_alg,
