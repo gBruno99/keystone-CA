df5702747ee395c8421681b42483f2acbca6dd0b
Giacomo <s301311@studenti.polito.it>
2023-10-15 15:02:57 +0200

    Added net and ref_certs files in server-CA

---
+++ server-CA/host/ref_certs.h
@@ -0,0 +1,10 @@
+#ifndef _HOST_REF_CERTS_H_
+#define _HOST_REF_CERTS_H_
+
+extern const unsigned char ref_cert_man[];
+extern const int ref_cert_man_len;
+
+extern const unsigned char sanctum_ca_private_key[];
+extern const unsigned char sanctum_ca_public_key[];
+
+#endif
