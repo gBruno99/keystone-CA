74d4b6850c3ea589874d2bb6c0a8933456447e91
Giacomo <s301311@studenti.polito.it>
2023-10-16 18:37:02 +0200

    Added CA cert - II

---
+++ enclave-Alice/eapp/ref_certs.h
@@ -0,0 +1,7 @@
+#ifndef _EAPP_REF_CERTS_H_
+#define _EAPP_REF_CERTS_H_
+
+extern const unsigned char ref_cert_ca[];
+extern const int ref_cert_ca_len;
+
+#endif
--- server-CA/host/ref_certs.h
+++ server-CA/host/ref_certs.h
@@ -6,5 +6,7 @@ extern const int ref_cert_man_len;
 
 extern const unsigned char sanctum_ca_private_key[];
 extern const unsigned char sanctum_ca_public_key[];
+extern const unsigned char sanctum_cert_ca[];
+extern const int sanctum_cert_ca_len;
 
 #endif
