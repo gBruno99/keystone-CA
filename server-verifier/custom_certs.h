c768be4315000cb281785e1353043e5251f1b5e8
Giacomo <s301311@studenti.polito.it>
2023-10-26 16:56:16 +0200

    Updated custom certs

--- server-verifier/custom_certs.h
+++ server-verifier/custom_certs.h
@@ -7,4 +7,10 @@ extern const unsigned long ca_cert_pem_len;
 extern const char ca_key_pem[];
 extern const unsigned long ca_key_pem_len;
 
+extern const char ver_cert_pem[];
+extern const unsigned long ver_cert_pem_len;
+
+extern const char ver_key_pem[];
+extern const unsigned long ver_key_pem_len;
+
 #endif
