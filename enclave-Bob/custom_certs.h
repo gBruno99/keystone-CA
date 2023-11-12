0f251fd441d20a6f6cb602fea86fe7f5389b4cdc
Giacomo <s301311@studenti.polito.it>
2023-11-12 07:39:22 +0100

    Added enclave-Bob

---
+++ enclave-Bob/custom_certs.h
@@ -0,0 +1,19 @@
+#ifndef _CUSTOM_CERTS_H_
+#define _CUSTOM_CERTS_H_
+
+extern const char ca_cert_pem[];
+extern const unsigned long ca_cert_pem_len;
+
+extern const char ca_key_pem[];
+extern const unsigned long ca_key_pem_len;
+
+extern const char ver_cert_pem[];
+extern const unsigned long ver_cert_pem_len;
+
+extern const char ver_key_pem[];
+extern const unsigned long ver_key_pem_len;
+
+extern const unsigned char ref_cert_man[];
+extern const int ref_cert_man_len;
+
+#endif
