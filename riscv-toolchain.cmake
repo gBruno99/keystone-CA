89415e475bf7df3f253444b00b08f8ca77f9efb9
Giacomo <s301311@studenti.polito.it>
2023-11-28 09:33:29 +0100

    Removed names dependencies

--- riscv-toolchain.cmake
+++ riscv-toolchain.cmake
@@ -1,9 +1,14 @@
+if (NOT DEFINED ENV{RISCV})
+  message(FATAL_ERROR "Please set RISCV environment variable to an installed RISCV path")
+endif()
+set(RISCV $ENV{RISCV})
+
 set(CMAKE_SYSTEM_NAME       Generic)
 
 set(CMAKE_C_COMPILER        riscv64-unknown-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER      riscv64-unknown-linux-gnu-g++)
 
-set(CMAKE_FIND_ROOT_PATH    /home/giacomo/Documents/keystone/riscv64/bin)
+set(CMAKE_FIND_ROOT_PATH    ${RISCV}/bin)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
